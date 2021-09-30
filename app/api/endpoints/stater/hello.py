748e075efcbc166138d987babcd7b6827bb2c6fd
natlove.natlove202@gmail.com <natlove.natlove202@gmail.com>
2021-09-30 14:51:30 +0700

    init project with fastapi

---
+++ app/api/endpoints/stater/hello.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+@router.get("/")
+def read_root():
+    return {"Hello": "World"}
---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from fastapi_pagination import add_pagination
+
+from app.api.api import api_router
+from app.core.config import settings
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+# add_pagination(app)
